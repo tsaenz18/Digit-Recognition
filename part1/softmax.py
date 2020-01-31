f956fed33dd4753403281b1c9c7db6eb3e373a6c
tsaenz18 <59756025+tsaenz18@users.noreply.github.com>
2020-01-30 16:23:04 -0800

    Update softmax.py

--- part1/softmax.py
+++ part1/softmax.py
@@ -91,18 +91,15 @@ def run_gradient_descent_iteration(X, Y, theta, alpha, lambda_factor, temp_param
         theta - (k, d) NumPy array that is the final value of parameters theta
     """
     #YOUR CODE HERE
-    theta_prime = np.copy(theta)
-    p = compute_probabilities(X, theta, temp_parameter)
-    for j in range(0, theta.shape[0]):
-        gradient = lambda_factor*theta[j,:]
-        n = Y.size
-        for i in range(0, n):
-            if Y[i] == j:
-                gradient -= X[i,:]*(1-p[j,i])/(n*temp_parameter)
-            else:
-                gradient -= X[i,:]*(-p[j,i])/(n*temp_parameter)
-        theta_prime[j,:] -= gradient*alpha
-    return theta_prime
+    itemp=1./temp_parameter
+    num_examples = X.shape[0]
+    num_labels = theta.shape[0]
+    probabilities = compute_probabilities(X, theta, temp_parameter)
+    # M[i][j] = 1 if y^(j) = i and 0 otherwise.
+    M = sparse.coo_matrix(([1]*num_examples, (Y,range(num_examples))), shape=(num_labels,num_examples)).toarray()
+    non_regularized_gradient = np.dot(M-probabilities, X)
+    non_regularized_gradient *= -itemp/num_examples
+    return theta - alpha * (non_regularized_gradient + lambda_factor * theta)
     raise NotImplementedError
 
 def update_y(train_y, test_y):
